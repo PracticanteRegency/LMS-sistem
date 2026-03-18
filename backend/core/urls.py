dd9750bf7197d6534926cafc7e74c9032e918937
PracticanteRegency <practicante.desarrollogh@regency.com.co>
2026-03-18 12:23:27 -0500

    feat: streamline media URL normalization and cleanup Celery beat schedule

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -33,16 +33,5 @@ urlpatterns = [
 # Servir archivos media en desarrollo
 from django.conf import settings
 from django.conf.urls.static import static
-
-# Servir archivos media SIEMPRE (no solo en DEBUG)
-# Necesario porque Nginx proxea /api/media/ al backend
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # En producción, servir media explícitamente ya que static() retorna [] con DEBUG=False
-    from django.views.static import serve
-    from django.urls import re_path
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
